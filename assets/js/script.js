ef1704fa3e6a0330f956cf42c875bc9c78fcac39
Ahmadalhindi <>
2023-07-09 17:29:59 +0000

    add the functions to javascript and declarations

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let buttons = document.getElementsByClassName("hero");
+let choices = ["rock", "paper", "scissors"];
+
+
+
+function playGame(userChoice) {
+
+
+    let userImage = document.getElementById("user-image");
+    let computerImage = document.getElementById("computer-image");
+
+    
+}
+
+
+function resultAndScore(computerChoice, userChoice) {
+
+}
+
