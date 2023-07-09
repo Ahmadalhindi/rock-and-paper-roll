e5139aaa9230955be96d3e7bfaf79b84cfdb7553
Ahmadalhindi <>
2023-07-09 18:26:25 +0000

    declare the user and cumputer choices and the result, return and alter their values by if coditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,10 @@ for (let button of buttons) {
         playGame(userChoice);
     });
 }
-
+/** 
+* The main game called when select the buttons
+* Declare and alter user and computer images according to choice 
+*/
 function playGame(userChoice) {
 
     let userImage = document.getElementById("user-image");
@@ -27,10 +30,42 @@ function playGame(userChoice) {
 
     resultAndScore(choices[computerChoice], choices[userChoice]);
 
-    console.log(result, "<===result")
+    console.log(result, "<===result");
 }
 
 
+/**Declare the user and cumputer choices and the result
+* Rtuern the result and score 
+* Alter the value of result, get the value of scores and increment it according to the conditions
+*/
 function resultAndScore(computerChoice, userChoice) {
+
+    let userScore = document.getElementById("user-score");
+    let computerScore = document.getElementById("computer-score");
+    let result = document.getElementById("result");
+
+    console.log(computerChoice, "<===comp choice");
+    console.log(userChoice, "<====player choice");
+
+    if (
+        userChoice === "paper" && computerChoice === "rock" ||
+        userChoice === "rock" && computerChoice === "scissors" ||
+        userChoice === "scissors" && computerChoice === "paper"
+    ) {
+        result.innerHTML = `You Score, ${userChoice} beats ${computerChoice}`;
+        (userScore.innerText)++ + 1;
+        return "You Score!";
+    } else if (
+        computerChoice === "paper" && userChoice === "rock" ||
+        computerChoice === "rock" && userChoice === "scissors" ||
+        computerChoice === "scissors" && userChoice === "paper"
+    ) {
+        result.innerHTML = `Computer Scores, ${computerChoice} beats ${userChoice}`;
+        (computerScore.innerText)++ + 1;
+        return "Computer Scores!";
+    } else {
+        result.innerText = "Show Mercy";
+        return "Nothing";
+    }
 }
 
