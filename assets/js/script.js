1030752612fa9d8b0198f7a43894011f26cab3a6
Ahmadalhindi <>
2023-07-09 17:59:15 +0000

    create th loop for main game, declare the user and computer choices and images, alter the images type after selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,36 @@
+// Get the choices and button elements and add event listener
 let buttons = document.getElementsByClassName("hero");
 let choices = ["rock", "paper", "scissors"];
 
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let userChoice = this.getAttribute("data-choice");
 
+        alert(`You clicked ${choices[userChoice]}`);
+
+        playGame(userChoice);
+    });
+}
 
 function playGame(userChoice) {
-
 
     let userImage = document.getElementById("user-image");
     let computerImage = document.getElementById("computer-image");
 
-    
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    userImage.src = `assets/images/${choices[userChoice]}.webp`;
+    userImage.alt = choices[userChoice];
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.webp`;
+    computerImage.alt = choices[computerChoice];
+
+    resultAndScore(choices[computerChoice], choices[userChoice]);
+
+    console.log(result, "<===result")
 }
 
 
 function resultAndScore(computerChoice, userChoice) {
-
 }
 
